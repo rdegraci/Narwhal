5a07898afb3cff3e46359d6233d44019836118db
Rodney Degracia <rodney@magicallightandsound.com>
2019-05-28 21:49:53 -0400

    Initial working telnet to socket_io code

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 // Load the TCP Library
 net = require('net');
+const io = require('socket.io-client')
+const uuidv1 = require('uuid/v1');
 
 // Keep track of the chat clients
 var clients = [];
+
+// Keep track of the socket.io connections
+
+var socket_io_connections = [];
+
+var socket2socketio = {};
+var socket_cache = {};
 
 // Start a TCP Server
 net.createServer(function(socket) {
@@ -12,6 +21,61 @@ net.createServer(function(socket) {
 
 	// Put this new client in the list
 	clients.push(socket);
+	
+	
+	
+	//// SOCKET IO
+
+	var socket_io = io.connect('https://acpt-barzoom.herokuapp.com', {reconnect: true});
+
+	socket_io_connections.push(socket_io);
+	
+	socket2socketio[socket.name] = socket_io;
+	socket_io.name = socket.name
+	socket_cache[socket.name] = socket
+
+	// Add a connect listener
+	socket_io.on('connect', function (ss) {
+    		console.log('Connected to Barzoom!\n');
+			
+			var socket = socket_cache[socket_io.name]
+			socket.write("Connected to Barzoom\n");
+	});
+
+	socket_io.on('disconnect', () => {
+		console.log("socketio on disconnect\n");
+		socket_io_connections.splice(clients.indexOf(socket_io), 1);
+		
+	});
+
+    socket_io.on('/pong', function(msg){
+		console.log("socketio on pong\n");
+		socket.write(msg);
+    });
+
+
+    socket.on('/shout', function(msg){
+		console.log("socketio on shout\n");
+		socket.write(msg);
+    });
+	
+
+    socket.on('/info', function(msg){
+		console.log("socketio on info\n");
+		socket.write(msg);
+    });
+	
+    socket.on('/notice', function(msg){
+		console.log("socketio on notice\n");
+		socket.write(msg);
+    });
+	
+    socket.on('/debug', function(msg){
+		console.log("socketio on debug\n");
+		socket.write(msg);
+    });
+
+	// SOCKETS
 
 	// Send a nice welcome message and announce
 	socket.write("Welcome " + socket.name + "\n");
@@ -19,16 +83,33 @@ net.createServer(function(socket) {
 
 	// Handle incoming messages from clients.
 	socket.on('data', function(data) {
-		if (data.String() === 'quit' || data.String() === 'exit' ) {
+		
+		if (data == null) {
+			return;
+		}
+		
+		if (data.toString() === 'quit' || data.toString() === 'exit' ) {
 			socket.close;
 			return;
-		} 
+		}
+		 
+		const params = data.toString().split(",");
+		const event = params[0];
+		const message = data.toString().split(/,(.+)/)[1];
+		
+		console.log("event =" + event + "message =" + message);
+		var socket_io = socket2socketio[socket.name]
+		socket_io.emit(event, message);
+		
 		broadcast(socket.name + "> " + data, socket);
 	});
 
 	// Remove the client from the list when it leaves
 	socket.on('end', function() {
+		console.log("socket on end\n");
 		clients.splice(clients.indexOf(socket), 1);
+		socket_io.disconnect();
+		socket_io_connections.splice(clients.indexOf(socket_io), 1);
 		broadcast(socket.name + " left the chat.\n");
 	});
 
